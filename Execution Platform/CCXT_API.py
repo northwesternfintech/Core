559f22490effb1445ad9acbde2bcfac7afe03986
Ethan Havemann <94627102+echavemann@users.noreply.github.com>
2022-09-30 20:47:45 -0600

    Update CCXT_API.py

--- Execution Platform/CCXT_API.py
+++ Execution Platform/CCXT_API.py
@@ -1,1 +1,13 @@
+import ccxt
+#import ccxt.async_support as ccxt
+
+class executionPlatform:
+  def __init__():
+    pass
+
+kraken = ccxt.kraken({
+    'apiKey': 'YOUR_PUBLIC_API_KEY',
+    'secret': 'YOUR_SECRET_PRIVATE_KEY',
+})
+
 
