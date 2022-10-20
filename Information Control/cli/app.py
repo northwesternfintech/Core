7e730cde1078bf2974e6f2a1a392c76857e3e237
ryan <rewong03@gmail.com>
2022-10-20 16:57:14 -0500

    Adding default folder for now

--- Information Control/cli/app.py
+++ Information Control/cli/app.py
@@ -1,8 +1,14 @@
+import os
+
 import cement
 
+from controllers.backtest import BacktestController
 from controllers.base import BaseController
 from controllers.web_socket import WebSocketController
-from controllers.backtest import BacktestController
+
+DIR_HOME = os.path.expanduser('~')
+DIR_NUFT = os.path.join(DIR_HOME, '.nuft')
+# DIR_LOG = os.path.join(DIR_NUFT, 'logs')
 
 
 class CoreCLI(cement.App):
@@ -10,6 +16,9 @@ class CoreCLI(cement.App):
         label = "nuft"
         extensions = ['tabulate']
         output_handler = 'tabulate'
+        config_files = [
+            os.path.join(DIR_NUFT, "creds.conf")
+        ]
         handlers = [
             BaseController,
             WebSocketController,
@@ -21,8 +30,18 @@ class CoreCLI(cement.App):
         # TODO: Add additional setup
 
 
-if __name__ == "__main__":
+def main():
+    if not os.path.exists(DIR_NUFT):
+        os.makedirs(DIR_NUFT, exist_ok=True)
+
+        with open(os.path.join(DIR_NUFT, "creds.conf"), "w"):
+            pass  # TODO: Add reasonable default
+
     with CoreCLI() as app:
         app.run()
+
+
+if __name__ == "__main__":
+    main()
 
    
