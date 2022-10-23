22ae5910ca9b5f5e91f5d55ec6ce5bd789f2addd
pl712 <liuzhengyao2011@gmail.com>
2022-10-22 20:23:02 -0500

    demo

--- Execution Platform/CCXT_API.py
+++ Execution Platform/CCXT_API.py
@@ -3,11 +3,20 @@ import ccxt
 
 class executionPlatform:
   def __init__():
-    pass
+    # self.exchangeInfo = {'binance':(binanceKey, binanceSecret, ccxt.binance()), 'bitmex':(bitmexKey, bitmexSecret)}
+    self.exchangeInfo = {'binance':(ccxt.binance({'apikey': binanceKey, 'secret': binanceSecret}))}
 
-kraken = ccxt.kraken({
-    'apiKey': 'YOUR_PUBLIC_API_KEY',
-    'secret': 'YOUR_SECRET_PRIVATE_KEY',
-})
+  def placeOrder(self, exchangeName, XXX):
+
+    exchangeKey, exchangeSec, exchangeObj = self.exchangeInfo[exchangeName]
+
+    # Place order
+
+
+
+# kraken = ccxt.kraken({
+#     'apiKey': 'YOUR_PUBLIC_API_KEY',
+#     'secret': 'YOUR_SECRET_PRIVATE_KEY',
+# })
 
 
