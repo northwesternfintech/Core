628719b53c65ccc8c392e6b6be110a3443809bb0
Richard Bann <richard@bann.com>
2022-10-29 16:04:37 -0500

    Revert "update CCXT"
    This reverts commit f3488b107d41d1099a628872fc9984080ba85455.

--- Execution Platform/CCXT_API.py
+++ Execution Platform/CCXT_API.py
@@ -1,70 +1,23 @@
+
 import ccxt
+#import ccxt.async_support as ccxt
 
-class executionPlatform():
-  def __init__(self):
-    load_dotenv()
-    self.mapToExchange = {}
+class executionPlatform:
+  def __init__():
+    # self.exchangeInfo = {'binance':(binanceKey, binanceSecret, ccxt.binance()), 'bitmex':(bitmexKey, bitmexSecret)}
+    self.exchangeInfo = {'binance':(ccxt.binance({'apikey': binanceKey, 'secret': binanceSecret}))}
 
-  def createExchange(self, exchangeName):
-    if exchangeName not in ccxt.exchanges:
-      raise Exception("ERROR>>Exchange Doesn't Exist in CCXT!")
-    self.mapToExchange[exchangeName] = ccxt.eval(exchangeName)()
-    self.mapToExchange[exchangeName].secret = eval(exchangeName)[0]
-    self.mapToExchange[exchangeName].apiKey = eval(exchangeName)[1]
+  def placeOrder(self, exchangeName, XXX):
 
-  def getBalance(self, exchange:str):
-    if exchange not in self.mapToExchange:
-      raise Exception("ERROR>>Exchange Not Created!")
-    else:
-      try:
-        return self.mapToExchange[exchange].fetchBalance()
+    exchangeKey, exchangeSec, exchangeObj = self.exchangeInfo[exchangeName]
 
-      except Exception as e: 
-        print(e)
+    # Place order
 
-  def placeOrder(self, exchange:str, tradeSymbol:str, tradeType:str, tradeSide:str, tradeAmount:int, price:float=None): #->return order ID
-    if exchange not in self.mapToExchange:
-      raise Exception("ERROR>>Exchange Not Created!")
-    else:
-      try:
-        if tradeType == 'limit':
-          orderBook = self.mapToExchange[exchange].createOrder(tradeSymbol, tradeType, tradeSide, tradeAmount, price)
-        elif tradeType == 'market':
-          orderBook = self.mapToExchange[exchange].createOrder(tradeSymbol, tradeType, tradeSide, tradeAmount)
 
-        return orderBook['id']
 
-      except Exception as e: 
-        print(e)
+# kraken = ccxt.kraken({
+#     'apiKey': 'YOUR_PUBLIC_API_KEY',
+#     'secret': 'YOUR_SECRET_PRIVATE_KEY',
+# })
 
-  def editOrder(self, exchange:str, orderID:str, tradeSymbol:str, tradeType:str, tradeSide:str, tradeAmount:int, price:float=None):
-    if exchange not in self.mapToExchange:
-      raise Exception("ERROR>>Exchange Not Created!")
-    else:
-      try:
-        if tradeType == 'limit':
-          return self.mapToExchange[exchange].editOrder(orderID, tradeSymbol, tradeType, tradeSide, tradeAmount, price)
-        elif tradeType == 'market':
-          return self.mapToExchange[exchange].editOrder(orderID, tradeSymbol, tradeType, tradeSide, tradeAmount)
-      except Exception as e: 
-        print(e)
 
-  def cancelOrder(self, exchange:str, tradingPair:str=None, orderID:list(str)=None):
-    if exchange == 'ALL':
-      if tradingPair != None:
-        for i in self.mapToExchange:
-          i.cancelAllOrders(tradingPair)
-      else:
-        for i in self.mapToExchange:
-          i.cancelAllOrders()
-    else:
-      if exchange not in self.mapToExchange:
-        raise Exception("ERROR>>Exchange Not Created!")
-      else:
-        try:
-          if tradingPair != None:
-            self.mapToExchange[exchange].cancelOrder(orderID, tradingPair)
-          else:
-            self.mapToExchange[exchange].cancelOrder(orderID)
-        except Exception as e: 
-          print(e)
