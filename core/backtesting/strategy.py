Fixed potential bug where trying to calculate standard deviation without first converting array to np array

core/backtesting/strategy.py
@@ -250,7 +250,7 @@ class Strategy(BackTester):
         return_differentials = [returns - riskfree for returns, riskfree in zip(self.daily_gain_loss, self.benchmark())]
         return_differentials = np.array(return_differentials)
         expected_differential = np.mean(return_differentials)
-        std = np.std(self.daily_gain_loss)
+        std = np.std(np.array(self.daily_gain_loss))
         assert std==0, "Standard deviation is 0. Cannot compute ratio."
         self.sharpe_ratio = expected_differential/std
         return True