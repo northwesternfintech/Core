682485cf19078922c79881f02083f4c1177e4a33
Akash Shroff <63399889+akashvshroff@users.noreply.github.com>
2023-01-07 20:29:48 -0600

    Added timestamp support and messages for orderLogs

--- core/execution_platform/log_class.py
+++ core/execution_platform/log_class.py
@@ -25,21 +25,27 @@ class LoggingClass:
 
     def errorLog(self, message):
         """
-        Logs any user error to the errorLogs folder
+        Logs any user error - with the timestamp and error message - to the errorLogs folder.
+        Returns 0 if logging is successful, 1 otherwise. 
         """
         try:
             errorFilePath = self.generateLogFilePath(True)
-            logging.basicConfig(filename=errorFilePath, filemode='w+', format='%(message)', level=logging.ERROR)
+            logging.basicConfig(filename=errorFilePath, filemode='w+', format='%(asctime)s - %(message)s', level=logging.ERROR, datefmt='%Y-%m-%d %H:%M:%S')
             logging.error(message)
             return 0
         except Exception as e:
             return 1
     
-    def orderLog(self, orderStructure):
+    def orderLog(self, orderStructure, message=""):
         """
-        Logs the order object into the csv dir
+        Logs the order object into the csv dir.
+        The fields of the csv file follow the keys of the order structure as defined in the API docs - 
+        https://docs.ccxt.com/en/latest/manual.html#order-structure
+        Contains an additional field 'result' which holds a string result of the transaction (if any). 
+        Returns 0 if logging is successful, 1 otherwise. 
         """
         try:
+            orderStructure['result'] = message
             orderLogPath = self.generateLogFilePath()
             file_exists = os.path.isfile(orderLogPath)
             with open("test.csv", 'w+', encoding='UTF8', newline='') as fhand:
