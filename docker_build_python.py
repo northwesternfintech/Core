7d1ce3100dbb62fae898a911dd1c5a003edc6e00
Richard Lam <richardlam2024@gmail.com>
2023-01-28 22:25:49 -0600

    Container successfully builds and runs

--- docker_build_python.py
+++ docker_build_python.py
@@ -6,30 +6,30 @@ def has_dockerfile(directory):
 
 def create_dockerfile(directory):
     with open(f"{directory}/Dockerfile", 'w') as f:
-        f.write("ENV FILE_PATH")
-        f.write('FROM PYTHON:3.10\n')
+        f.write('FROM python:3.10\n')
+        f.write('ARG FILE_PATH\n')
+        f.write("ENV FILE_PATH $FILE_PATH\n")
         f.write('COPY . /app\n')
         f.write('WORKDIR /app\n')
         
-        # Edit to include different types of configuration files...! 
-        f.write('RUN pip3 install --upgrade pip3 && \
+        f.write('RUN apt-get update && apt-get install -y python3.10 python3-pip &&\
                  test -f requirements.txt && pip3 install -r requirements.txt || true && \
                  test -f Pipfile.lock && pipenv install --ignore-pipfile || true && \
-                 test -f setup.py && python3 setup.py || true\n')
-        f.write(f"CMD python3 $FILE_PATH\n")
+                 test -f setup.py && pip3 install setup.py || true\n')
+        f.write("CMD python3 $FILE_PATH\n")
 
-def build_docker_image(directory, file_name, image_name):
-    subprocess.run(['docker', 'build', "--build-arg", f"FILE_PATH={file_name}", '-t', image_name, directory])
+def build_docker_image(directory, file_path, image_name):
+    subprocess.run(['docker', 'build', "--build-arg", f"FILE_PATH={file_path}", '-t', image_name, directory])
 
-def run_docker_image(image_name, file_name):
-    subprocess.run(['docker', 'run', "-e", f"FILE_PATH=${file_name}", image_name])
+def run_docker_image(image_name, file_path):
+    subprocess.run(['docker', 'run', "-e", f"FILE_PATH={file_path}", image_name])
 
-
-# Call the function at the top of Core.
-def repo_to_container(directory, file_name, image_name):
-
+def repo_to_container(directory, file_path, image_name):
     if not has_dockerfile(directory):
         create_dockerfile(directory)
     
-    build_docker_image(image_name, file_name, image_name)
-    run_docker_image(image_name, file_name)
+    build_docker_image(directory, file_path, image_name)
+    run_docker_image(image_name, file_path)
+
+
+repo_to_container("../Desk-Alpha-Fork/Strats/BollingerBands", "BollingerBandsMultiStock.py", "test")
